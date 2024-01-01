d0212acd0a592b0e23e763ad66c803041d1b4a6b
r3dnbl4ck <arbongeci@protonmail.ch>
2024-01-01 22:43:50 +0100

    r

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,28 @@
 <script>
-    import '@picocss/pico'
-    import '../app.css'
+	import { invalidateAll } from '$app/navigation'
+	import '@picocss/pico';
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { supabaseClient } from '$lib/supabase' 
+
+	onMount(() => {
+		const { data : { subscription }} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidateAll()
+		})
+
+		return () => {
+			subscription.unsubscribe()
+		}
+	})
 </script>
+
+<nav aria-label="breadcrumb" style="--pico-nav-breadcrumb-divider: '/';">
+	<!-- svelte-ignore a11y-invalid-attribute -->
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Booster</a></li>
+		<li><a href="#">My Account</a></li>
+	</ul>
+</nav>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// let steamGuard = 'Not Provided';
+    import { Icon, Pencil } from 'svelte-hero-icons'
+
+	let users = [
+		{
+			username: 'smoqi',
+			password: '*******',
+			// steamGuard: steamGuard
+		},
+		{
+			username: 'madcentt',
+			password: '*******',
+			// steamGuard: steamGuard
+		},
+		{
+			username: 'boosterUnit',
+			password: '*******',
+			// steamGuard: steamGuard
+		}
+	];
+</script>
+
+<main>
+	<div class="container">
+		<hr />
+		<h3>unitks booster /</h3>
+
+		<article>
+			<h5>My Accounts</h5>
+			<table>
+				<thead>
+					<tr>
+						<th scope="col">Username</th>
+						<th scope="col">Password</th>
+						<th scope="col">Steam Guard</th>
+						<th scope="col">#</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each users as user}
+						<tr>
+							<th scope="row">{user.username}</th>
+							<td>{user.password} <Icon src="{Pencil}" size="20" /></td>
+							<td><button class="secondary">Enter Steam Guard</button></td>
+							<td>
+                                <button class="primary">Start</button>
+								<button class="outline secondary">Delete</button>
+                            </td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</article>
+	</div>
+</main>
